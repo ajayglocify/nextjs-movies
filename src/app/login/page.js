898d41c0ae5d44ef4cbb9b2c351d6f1b9ddd5abb
ajayglocify <ajay@glocify.com>
2024-07-25 17:16:14 +0530

    login ui change
    login ui change

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,84 +1,169 @@
 "use client";
+/*
 import 'bootstrap/dist/css/bootstrap.css';
 import {useForm} from "react-hook-form";
 import axios from 'axios';
 import {useRouter } from 'next/navigation';
 import './../../../public/assets/css/login.module.css';
-import Cookies from 'js-cookie';
+
 import Image from 'next/image';
 
+*/
 
-export default function Login() {
-  const { register, handleSubmit } = useForm();
+/* mui login imports */
+import axios from 'axios';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Cookies from 'js-cookie';
+import {useRouter } from 'next/navigation';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {useForm} from "react-hook-form";
+import { styled } from '@mui/material/styles';
+
+/* mui login end */
+
+
+const defaultTheme = createTheme();
+
+
+export default function SignIn() {
+  const {register,handleSubmit,formState: { errors }} = useForm();
   const router = useRouter();
-  const Data = (data) =>{
 
-      axios.post('/api/login',data)
-      .then(function (response) {
-        if(response.status === 200){
-          alert(response.data.message);
-          Cookies.set('AuthUser',response.data.token, { expires: 7 });
-          router.push('/movies');
-        }
-      })
-      .catch(function (error) {
-        alert(error);
-      });      
-  }
+  const CustomTextField = styled(TextField)({
+    '& .MuiInputBase-input::placeholder': {
+      color: '#fff', // Set the placeholder text color here
+      opacity: 1, // Ensure the color is not translucent
+    },
+  });
+
+  const handleSubmitForm = (data) => {
+    axios.post('/api/login',data)
+    .then(function (response) {
+      if(response.status === 200){
+        alert(response.data.message);
+        Cookies.set('AuthUser',response.data.token, { expires: 7 });
+        router.push('/movies');
+      }
+    })
+    .catch(function (error) {
+      alert(error);
+    });  
+  };
+
   return (
-    <main className="container">
-    <section className="h-100 gradientform" style={{ backgroundColor: "#eee" }}>
-
-        <div className="container py-5 h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-xl-10">
-              <div className="card rounded-3 text-black">
-                <div className="row g-0">
-                  <div className="col-lg-6">
-                    <div className="card-body p-md-5 mx-md-4">
-
-                      <div className="text-center">
-                        <Image src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp" width={185} height={150}
-                          style={{width: "185px"}} alt="logo" />
-                        <h4 className="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
-                      </div>
-
-                      <form onSubmit={handleSubmit(Data)}>
-                        <p>Please login to your account</p>
-
-                        <div data-mdb-input-init className="form-outline mb-4">
-                          <input {...register("emails", { required: true })} type="text" id="form2Example11" className="form-control"
-                            placeholder="Phone number or email address" />
-                          <label className="form-label" for="form2Example11">Username</label>
-                        </div>
-
-                        <div data-mdb-input-init className="form-outline mb-4">
-                          <input type="password"  {...register("passwords", { required: true })} id="form2Example22" className="form-control" />
-                          <label className="form-label" for="form2Example22">Password</label>
-                        </div>
-
-                        <div className="text-center pt-1 mb-5 pb-1">
-                          <input  data-mdb-button-init data-mdb-ripple-init className={`btn btn-primary btn-block fa-lg gradientCustom2 mb-3`}  type="submit" value="Login" />
-                        </div>
-
-                      </form>
-
-                    </div>
-                  </div>
-                  <div className={`col-lg-6 d-flex align-items-center gradientCustom2`}>
-                    <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-                      <h4 className="mb-4">We are more than just a company</h4>
-                      <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </main>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h3">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit(handleSubmitForm)} noValidate sx={{ mt: 1 }}>
+            <TextField
+              sx={{
+                color: '#fff',
+                border: '1px solid transparent',
+                borderRadius : '10px',
+                backgroundColor : '#224957',
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': {
+                    borderColor: 'transparent', // Make sure the border is transparent
+                  },
+                  '&:hover fieldset': {
+                    borderColor: 'transparent', // Ensure no border on hover
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: 'transparent', // Ensure no border on focus
+                  },
+                }
+              }}
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              placeholder="Email Address"
+              autoComplete="email"
+              autoFocus
+              {...register('emails', {
+                required: 'Email is required', // Custom error message
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, // Email regex pattern
+                  message: 'Invalid email address', // Custom error message
+                },
+              })}
+            />
+            {errors.emails && <span className='text-danger'>{errors.emails.message}</span> }
+            <TextField
+              sx={{
+                            color: '#fff',
+                            border: '1px solid transparent',
+                            borderRadius : '10px',
+                            backgroundColor : '#224957',
+                            '& .MuiOutlinedInput-root': {
+                              '& fieldset': {
+                                borderColor: 'transparent', // Make sure the border is transparent
+                              },
+                              '&:hover fieldset': {
+                                borderColor: 'transparent', // Ensure no border on hover
+                              },
+                              '&.Mui-focused fieldset': {
+                                borderColor: 'transparent', // Ensure no border on focus
+                              },
+                            }
+              }}
+              margin="normal"
+              required
+              fullWidth
+              placeholder="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              {...register("passwords", { required: 'Password is Required.' })}
+            />
+             {errors.passwords && <span className='text-danger'>{errors.passwords.message}</span> }
+             <div class="text-center "> 
+             <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+             </div>
+             <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  backgroundColor: '#2BD17E',
+                  ':hover': {
+                    backgroundColor: 'transparent',
+                    color: '#fff',
+                    border : '1px solid #fff'
+                  },
+                }}
+              >
+                Sign In
+              </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
